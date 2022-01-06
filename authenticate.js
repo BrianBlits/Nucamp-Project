471522511bcf862af410ed6b2be81071e799eb79
Blits <brianlapeyrouse98@gmail.com>
2022-01-06 10:31:00 -0600

    HTTPS

--- authenticate.js
+++ authenticate.js
@@ -44,18 +44,12 @@ exports.jwtPassport = passport.use(
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
 
-exports.verifyAdmin = ((req,res, next) => {
-    User.findById({_id: req.user._id})
-    .then((user) => {
-        if (user.admin == true){
-            console.log("This admin is a user", user.admin);
-            next();
-        }
-        else {
-            err = new Error("You are not authorized to perform this operation!");
-            res.statusCode = 403;
-            return next(err);
-        }
-    }, (err) => next(err))
-    .catch((err) => next(err));
-})
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin){
+    return next();
+    } else {
+    const err = new Error('You are not authorized to perform this operation!');
+    err.status = 403 ;
+    return next(err);
+    }
+};
